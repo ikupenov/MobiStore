b5c5cada8e8327a360820db4db09c1ec9fdb0bca
Iliyan Kupenov <dmxyyyy@gmail.com>
2016-10-18 03:25:44 +0300

    XmlImporter implemented

---
+++ Source/MobiStore.Utils/Importers/XmlImporter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using MobiStore.Data.Contracts;
+using MobiStore.Utils.Contracts;
+
+namespace MobiStore.Utils.Importers
+{
+    public class XmlImporter<T> : IXmlImporter<T> where T : class
+    {
+        private IRepository<T> repository;
+
+        public XmlImporter(IRepository<T> repository)
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException(
+                    $"{this.GetType().Name} constructor parameter {repository.GetType().Name} cannot be null.");
+            }
+
+            this.repository = repository;
+        }
+
+        public void Import(T fileToImport)
+        {
+            this.repository.Add(fileToImport);
+        }
+    }
+}
