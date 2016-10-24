4d073d9bc18126db8374ab1e4cfb5d19f00d887a
Iliyan Kupenov <dmxyyyy@gmail.com>
2016-10-24 16:42:37 +0300

    bug fixes

--- MobiStore/Source/MobiStore.DesktopClient/Properties/Resources.Designer.cs
+++ MobiStore/Source/MobiStore.DesktopClient/Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace MobiStore.DesktopClient.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap canary_islands_sunset_wide {
-            get {
-                object obj = ResourceManager.GetObject("canary_islands_sunset-wide", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
